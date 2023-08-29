d8d9f871b81d86b7bfe1db559e8792c4d18c52b5
rajput-akshay03 <akshayrajput152003@gmail.com>
2023-08-29 22:08:02 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -14,5 +14,33 @@ app.use(cors());
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
 app.use('/api/message',messageRoutes);
-app.listen(port, console.log(`running one ${port}`))
+const server = app.listen(port, console.log(`running one ${port}`))
+const io = require("socket.io")(server,{
+    pingTimeout:60000,
+    cors:{
+        origin:"http://localhost:3000"
+    }
+});
+io.on("connection",(socket)=>{
+    console.log("connectd to socket.io");
+    socket.on("setup",(userData)=>{
+        socket.join(userData._id);
+        socket.emit("connected");
+    })
+    socket.on("join chat",(room)=>{
+        socket.join(room);
+    })
+    socket.on("newMessage",(newMessageRecieved)=>{
+        console.log("hello")
+        var chat = newMessageRecieved.message.chat;
+        if (!chat.users) return console.log("user not found");
+        chat.users.forEach((user) => { 
+             if(user._id==newMessageRecieved.message.sender._id)  return;
+                //   socket.in(user._id).emit("message recieved",newMessageRecieved.message.chat)
+                socket.in(user._id).emit("message recieved",newMessageRecieved.message)
+        });
+    })
+    socket.on("typing",(room)=>{socket.in(room).emit("typing")})
+    socket.on("stop typing",(room)=>{socket.in(room).emit("stop typing")})
+})
 app.get('/',(req,res)=>{res.send(`running on ${port}`)})
--- frontend/src/components/myMessages.js
+++ frontend/src/components/myMessages.js
@@ -2,14 +2,56 @@ import { useContext, useEffect, useState } from "react";
 import { Chatcontext } from "../context/ChatProvider";
 import axios from "axios";
 import './css/message.css'
-
+import io from "socket.io-client"
+const ENDPOINT = "http://localhost:4000";
+var socket,selectChatCompare;
 const MyMessages = ()=>{
     const [messages,setmessage]= useState([]);
     const [newmessage,setnewmessage]= useState();
     const {user,selectedChat,setSelectedChat,chats,setChats} = useContext(Chatcontext);
+    const [socketConnected,setSocketConnected] = useState(false);
+    const [typing,setTyping] = useState(false);
+    const [isTyping,setIsTyping] = useState(false);
+
+    useEffect(()=>{
+        socket = io(ENDPOINT);
+        if(user!=null)
+       { socket.emit("setup",user);}
+        socket.on("connected",()=>{setSocketConnected(true) ;})
+        socket.on("typing",()=>setIsTyping(true))
+        socket.on("stop typing",()=>setIsTyping(false))
+   },[user])
     function inputmessage(e)
     {
         setnewmessage(e.target.value);
+        console.log(e.target.value)
+        if(e.target.value=="")
+        {   
+            socket.emit("stop typing",selectedChat._id);
+               setTyping(false)
+        }
+        if(!socketConnected)
+        {  
+            return ;
+        }
+       if(!typing)
+       {
+           setTyping(true);
+           socket.emit("typing",selectedChat._id);
+       }
+       let lastTypingTime = new Date().getTime();
+       var timerLength = 3000;
+       setTimeout(()=>{
+           var timeNow = new Date().getTime();
+           var timeDiff = timeNow - lastTypingTime;
+           console.log(timeDiff)
+           if(timeDiff>=timerLength&&typing)
+           {
+             console.log("akshay")
+               socket.emit("stop typing",selectedChat._id);
+               setTyping(false)
+           }
+       })
     }
     const fetchmessages = async()=>{
         if(!selectedChat) return;
@@ -20,19 +62,38 @@ const MyMessages = ()=>{
                 }
             }
             const {data} = await axios.get(`http://localhost:4000/api/message/${selectedChat._id}`,config);
-           
-           setmessage(data)
+            setmessage(data);
+            socket.emit("join chat",selectedChat._id)
         }
         catch(err)
         {
               console.log(err)
         }
     }
-    useEffect(()=>{fetchmessages()},[selectedChat])
+    useEffect(()=>{fetchmessages();
+        selectChatCompare= selectedChat;
+    }
+    ,[selectedChat]);
+    useEffect(()=>{
+        socket.on("message recieved",(newMessageRecieved)=>{
+            if(!selectChatCompare||selectChatCompare._id != newMessageRecieved.chat._id)
+            {
+                // notifications
+               
+            }
+            else{
+                setmessage([...messages,newMessageRecieved])
+            }
+        })
+  
+    })
     const sendmessage = async(e)=>
     {     
           if(e.key=="Enter" &&  newmessage)
           {
+            {socket.emit("stop typing",selectedChat._id);
+              setTyping(false)
+        }
                try{
                     const config = {
                         headers:{
@@ -46,10 +107,9 @@ const MyMessages = ()=>{
                                chatId:selectedChat._id
                             } ,config                
                     )
-                   
                      setmessage([...messages,data.message]);
+                     socket.emit("newMessage",data);
                      e.target.value=""
-                     console.log(messages)
                }
                catch(err)
                {
@@ -66,7 +126,7 @@ const MyMessages = ()=>{
                 <div>
                       <div>
                         {
-                            messages.map((msg)=>(<div key={msg.id} className={msg.sender._id==user._id?"aks":"aks2"}>{console.log(msg.sender._id)}{msg.content}</div>))
+                            messages.map((msg)=>(<div key={msg.id} className={msg.sender._id==user._id?"aks":"aks2"}>{console.log("hilla")}{msg.content}</div>))
                         }
                      </div>
                 </div>
@@ -74,6 +134,9 @@ const MyMessages = ()=>{
                 <div className="selectchat">Please Select Chat !</div>
             }
             </div>
+            {
+                isTyping?<div>loading...</div>:null
+            }
              <div className="inputfield">
             <input type="text" placeholder="enter message here" onChange={inputmessage} onKeyDown={sendmessage} className="input"/>
             </div>
